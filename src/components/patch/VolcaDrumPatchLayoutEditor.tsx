c632f71204cd2a944739536d534f73878cea1a97
Umberto Loria <umberto.loria@gmail.com>
2025-03-02 20:07:31 +0100

    Volca Drum Patch Editor manages Modulation Type and Amp Eg

--- src/components/patch/VolcaDrumPatchLayoutEditor.tsx
+++ src/components/patch/VolcaDrumPatchLayoutEditor.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 import {
   YamlPatchLayout,
+  YamlPatchLayoutAmpEg,
+  YamlPatchLayoutModulationType,
   YamlPatchLayoutSoundSrcType,
 } from '../../lib/volca_drum_patch.ts'
 
@@ -24,6 +26,24 @@ export const VolcaDrumPatchLayoutEditor: FC<{
               })
             }}
           />
+          <InputModulationType
+            modulationType={layout.mod_type}
+            setModulationType={newValue => {
+              setLayout({
+                ...layout,
+                mod_type: newValue,
+              })
+            }}
+          />
+          <InputAmpEg
+            ampEg={layout.amp_eg}
+            setAmpEg={newValue => {
+              setLayout({
+                ...layout,
+                amp_eg: newValue,
+              })
+            }}
+          />
         </fieldset>
       </form>
     </>
@@ -40,48 +60,132 @@ const InputSoundSourceType: FC<{
         <label>Sound Source Type</label>
         <div>
           <RadioLabel
-            name='sound_source_type'
-            value='WaveSine'
-            label='WaveSine'
+            name="sound_source_type"
+            value="WaveSine"
+            label="WaveSine"
             checked={sourceSrcType == 'WaveSine'}
             onChecked={() => {
               setSourceSrcType('WaveSine')
             }}
           />
           <RadioLabel
-            name='sound_source_type'
-            value='WaveSaw'
-            label='WaveSaw'
+            name="sound_source_type"
+            value="WaveSaw"
+            label="WaveSaw"
             checked={sourceSrcType == 'WaveSaw'}
             onChecked={() => {
               setSourceSrcType('WaveSaw')
             }}
           />
           <RadioLabel
-            name='sound_source_type'
-            value='WaveNoiseHPF'
-            label='WaveNoiseHPF'
+            name="sound_source_type"
+            value="WaveNoiseHPF"
+            label="WaveNoiseHPF"
             checked={sourceSrcType == 'WaveNoiseHPF'}
             onChecked={() => {
               setSourceSrcType('WaveNoiseHPF')
             }}
           />
           <RadioLabel
-            name='sound_source_type'
-            value='WaveNoiseLPF'
-            label='WaveNoiseLPF'
+            name="sound_source_type"
+            value="WaveNoiseLPF"
+            label="WaveNoiseLPF"
             checked={sourceSrcType == 'WaveNoiseLPF'}
             onChecked={() => {
               setSourceSrcType('WaveNoiseLPF')
             }}
           />
           <RadioLabel
-            name='sound_source_type'
-            value='WaveNoiseBPF'
-            label='WaveNoiseBPF'
+            name="sound_source_type"
+            value="WaveNoiseBPF"
+            label="WaveNoiseBPF"
             checked={sourceSrcType == 'WaveNoiseBPF'}
             onChecked={() => {
               setSourceSrcType('WaveNoiseBPF')
+            }}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+const InputModulationType: FC<{
+  modulationType: YamlPatchLayoutModulationType
+  setModulationType: (modulationType: YamlPatchLayoutModulationType) => void
+}> = ({ modulationType, setModulationType }) => {
+  return (
+    <>
+      <div>
+        <label>Modulation Type</label>
+        <div>
+          <RadioLabel
+            name="mod_type"
+            value="ModExp"
+            label="ModExp"
+            checked={modulationType == 'ModExp'}
+            onChecked={() => {
+              setModulationType('ModExp')
+            }}
+          />
+          <RadioLabel
+            name="mod_type"
+            value="ModTri"
+            label="ModTri"
+            checked={modulationType == 'ModTri'}
+            onChecked={() => {
+              setModulationType('ModTri')
+            }}
+          />
+          <RadioLabel
+            name="mod_type"
+            value="ModRand"
+            label="ModRand"
+            checked={modulationType == 'ModRand'}
+            onChecked={() => {
+              setModulationType('ModRand')
+            }}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+const InputAmpEg: FC<{
+  ampEg: YamlPatchLayoutAmpEg
+  setAmpEg: (ampEg: YamlPatchLayoutAmpEg) => void
+}> = ({ ampEg, setAmpEg }) => {
+  return (
+    <>
+      <div>
+        <label>Amp Eg</label>
+        <div>
+          <RadioLabel
+            name="amp_eg"
+            value="EnvAd"
+            label="EnvAd"
+            checked={ampEg == 'EnvAd'}
+            onChecked={() => {
+              setAmpEg('EnvAd')
+            }}
+          />
+          <RadioLabel
+            name="amp_eg"
+            value="EnvExp"
+            label="EnvExp"
+            checked={ampEg == 'EnvExp'}
+            onChecked={() => {
+              setAmpEg('EnvExp')
+            }}
+          />
+          <RadioLabel
+            name="amp_eg"
+            value="EnvMul"
+            label="EnvMul"
+            checked={ampEg == 'EnvMul'}
+            onChecked={() => {
+              setAmpEg('EnvMul')
             }}
           />
         </div>
@@ -102,7 +206,7 @@ const RadioLabel: FC<{
       <div>
         <label>
           <input
-            type='radio'
+            type="radio"
             name={name}
             value={value}
             checked={checked}
